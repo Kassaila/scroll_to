262097de108491ba261af407353cd8949ad27120
Kassaila <dmytro.winter@gmail.com>
2018-10-10 23:02:54 +0300

    Fix function usege

--- js/scroll_to.js
+++ js/scroll_to.js
@@ -3,34 +3,33 @@ $(window).on('load', function () {
     $('.preloader').delay(600).fadeOut('slow');
     $('.preloader').children().fadeIn('normal').fadeOut('normal');
 });
-/* "init_page_position" (top or bottom) */
-let docHeight = $('html').height(),
-    scrollBottom = () => {
-        return $(window).scrollTop() + screen.availHeight;
-    },
-    initPagePos = () => {
-        if (screen.availHeight * 0.75 > $(window).scrollTop()) {
-            $('body').addClass('page_top');
-        } else if (docHeight - screen.availHeight * 0.75 < scrollBottom()) {
-            $('body').addClass('page_bottom');
-        } else {
-            $('body').removeClass('page_top page_bottom');
-            if ($('body').attr('class') === '') {
-                $('body').removeAttr('class');
-            }
+/* "page_position" (top or bottom) */
+function scrollBottom() {
+    return $(window).scrollTop() + screen.availHeight;
+}
+function initPagePos() {
+    if (screen.availHeight * 0.75 > $(window).scrollTop()) {
+        $('body').addClass('page_top');
+    } else if ($('html').height() - screen.availHeight * 0.75 < scrollBottom()) {
+        $('body').addClass('page_bottom');
+    } else {
+        $('body').removeClass('page_top page_bottom');
+        if ($('body').attr('class') === '') {
+            $('body').removeAttr('class');
         }
     }
-$(window).resize(() => { docHeight = $('html').height(); })
-$(document).ready(() => { initPagePos(); });
-$(window).scroll(() => { initPagePos(); });
+}
+$(document).ready(initPagePos());
+$(window).scroll(initPagePos());
 /* "scroll_to", "on_top_button" */
 $('.scroll_to').bind('click', function (e) {
     let anchor = $($(this).attr('href')).offset().top,
         timeRate = Math.round(Math.abs($(window).scrollTop() - anchor) / screen.availHeight);
     if (screen.availHeight * 0.75 < Math.abs($(window).scrollTop() - anchor)) {
-        if (timeRate > 2) {
+        if (timeRate > 2 && $('html').find('.preloader').length !== 0) {
             $('.preloader').delay(0).fadeIn('slow');
             $('.preloader').delay(timeRate * 300).fadeOut('slow');
+            console.log($('html').find('.preloader').length);
         }
         $('html, body').stop().animate({
             scrollTop: anchor
