2052156f7846bbe7b2f8aa96bd55043e1970a6fe
Kassaila <dmytro.winter@gmail.com>
2019-01-05 22:51:38 +0100

    Fix speed of scroll & functions

--- js/scroll_to.js
+++ js/scroll_to.js
@@ -7,34 +7,46 @@ $(window).on('load', function () {
 function scrollBottom() {
     return $(window).scrollTop() + screen.availHeight;
 }
+
 function initPagePos() {
     if (screen.availHeight * 0.75 > $(window).scrollTop()) {
-        $('body').addClass('page_top');
+        $('html').addClass('page_top');
     } else if ($('html').height() - screen.availHeight * 0.75 < scrollBottom()) {
-        $('body').addClass('page_bottom');
+        $('html').addClass('page_bottom');
     } else {
-        $('body').removeClass('page_top page_bottom');
-        if ($('body').attr('class') === '') {
-            $('body').removeAttr('class');
+        $('html').removeClass('page_top page_bottom');
+        if ($('html').attr('class') === '') {
+            $('html').removeAttr('class');
         }
     }
 }
-$(document).ready(initPagePos());
-$(window).scroll(initPagePos());
+
+$(document).ready(function () {
+    initPagePos();
+});
+$(window).scroll(function () {
+    initPagePos();
+});
+$(window).resize(function () {
+    initPagePos();
+});
 /* "scroll_to", "on_top_button" */
 $('.scroll_to').bind('click', function (e) {
     let anchor = $($(this).attr('href')).offset().top,
         timeRate = Math.round(Math.abs($(window).scrollTop() - anchor) / screen.availHeight);
     if (screen.availHeight * 0.75 < Math.abs($(window).scrollTop() - anchor)) {
         if (timeRate > 2 && $('html').find('.preloader').length !== 0) {
-            $('.preloader').delay(0).fadeIn('slow');
-            $('.preloader').delay(timeRate * 300).fadeOut('slow');
+            $('.preloader').delay(0).fadeIn('normal');
+            $('.preloader').delay(500).fadeOut('slow');
+            $('html, body').stop().animate({
+                scrollTop: anchor
+            }, 1500);
+        } else {
+            $('html, body').stop().animate({
+                scrollTop: anchor
+            }, timeRate * 500);
         }
-        $('html, body').stop().animate({
-            scrollTop: anchor
-        }, timeRate * 500);
-        /*console.log(`anchor:${anchor}, time rate:${timeRate};
-main delay:${timeRate * 500},pre delay:${timeRate * 300}`);*/
+        /*console.log(`anchor:${anchor}, time rate:${timeRate}`);*/
     }
     e.preventDefault();
 });
