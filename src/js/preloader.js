37f614b769416d43c4ccff297c2d58919dcf5f35
Kassaila <dmytro.winter@gmail.com>
2019-03-24 16:41:04 +0100

    Reorganisation files

--- src/js/preloader.js
+++
@@ -1,8 +0,0 @@
-// "preloader"
-function preloader(setProp) {
-    // Default delay | Задержка по умолчанию
-    setProp.delay = setProp.delay !== undefined && typeof setProp.delay === 'number' && setProp.delay >= 0 ? setProp.delay : 600;
-    $(window).on('load', () => {
-        $('#' + setProp.preloaderId).delay(setProp.delay).fadeOut('slow');
-    });
-}
--- src/js/scroll_to.js
+++ src/js/scroll_to.js
@@ -2,53 +2,58 @@
  * {s} symonov.com
  * 2018
  */
-// "scroll_to"
+// scroll to
 function scrollTo(setProp) {
     // Default options | Настройки по умолчанию
-    setProp.initPosition = setProp.initPosition !== undefined && typeof setProp.initPosition === 'boolean' ? setProp.initPosition : true;
-    setProp.initScrHeight = setProp.initScrHeight !== undefined && typeof setProp.initScrHeight === 'number' && setProp.initScrHeight >= 0.1 && setProp.initScrHeight <= 1 ? setProp.initScrHeight : 0.75;
-    // "page_position" (top or bottom)
-    let scrollBottom = () => $(window).scrollTop() + screen.availHeight,
-        initPagePos = () => {
-            if (screen.availHeight * setProp.initScrHeight > $(window).scrollTop()) {
-                $('html').addClass('page_top');
-            } else if ($('html').height() - screen.availHeight * setProp.initScrHeight < scrollBottom()) {
-                $('html').addClass('page_bottom');
-            } else {
-                $('html').removeClass('page_top page_bottom');
-                if ($('html').attr('class') === '') {
-                    $('html').removeAttr('class');
-                }
-            }
-        }
-    // init "page_position" on events
-    if (setProp.initPosition) {
-        $(document).ready(() => initPagePos());
-        $(window).scroll(() => initPagePos());
-        $(window).resize(() => initPagePos());
-    }
-    // Default options | Настройки по умолчанию
-    setProp.scrollClass = setProp.scrollClass !== undefined && typeof setProp.scrollClass === 'string' && setProp.scrollClass !== '' ? setProp.scrollClass : 'scroll_to';
     setProp.scrollDelay = setProp.scrollDelay !== undefined && typeof setProp.scrollDelay === 'number' && setProp.scrollDelay >= 0 ? setProp.scrollDelay : 600;
+    setProp.anchorURL = setProp.anchorURL !== undefined && typeof setProp.anchorURL === 'boolean' ? setProp.anchorURL : true;
     setProp.preloader = setProp.preloader !== undefined && typeof setProp.preloader === 'boolean' ? setProp.preloader : false;
-    // "scroll_to", "on_top_button"
-    $('.' + setProp.scrollClass).bind('click', function (e) {
-        let anchor = $($(this).attr('href')).offset().top,
+    //scroll to anchor
+    let scrollToAnchor = (target) => {
+        let anchor = $(target).offset().top,
             timeRate = Math.round(Math.abs($(window).scrollTop() - anchor) / screen.availHeight);
         if (screen.availHeight * 0.75 < Math.abs($(window).scrollTop() - anchor)) {
             if (setProp.preloader && $('html').find('#' + setProp.preloaderId).length !== 0 && timeRate > 2) {
-                $('#' + setProp.preloaderId).fadeIn('normal');
-                $('#' + setProp.preloaderId).delay(setProp.scrollDelay * 2 + 300).fadeOut('slow');
+                $('#' + setProp.preloaderId).fadeIn('normal').delay(setProp.scrollDelay * 2 + 300).fadeOut('slow');
                 $('html, body').stop();
                 setTimeout(() => $('html, body').animate({
                     scrollTop: anchor
                 }, setProp.scrollDelay * 2), 300);
+                return setProp.scrollDelay * 2 + 300;
             } else {
                 $('html, body').stop().animate({
                     scrollTop: anchor
-                }, timeRate * setProp.scrollDelay);
+                }, setProp.scrollDelay * timeRate);
+                return setProp.scrollDelay * timeRate;
             }
         }
-        e.preventDefault();
+    }
+    // current page scroll to anchor
+    $('a').bind('click', function (event) {
+        if ($(this)['0'].pathname === window.location.pathname) {
+            event.preventDefault();
+            let currentPageId = $(this).attr('href'),
+                timeToAnchor = scrollToAnchor(currentPageId);
+            if (setProp.anchorURL === true) {
+                setTimeout(() => {
+                    window.location.hash = currentPageId;
+                }, timeToAnchor);
+            }
+        }
     });
+    // other page scroll to anchor
+    otherPageId = window.location.hash;
+    if (otherPageId !== '') {
+        window.location.hash = '';
+        $(document).ready(() => {
+            setTimeout(() => {
+                let timeToAnchor = scrollToAnchor(otherPageId);
+                if (setProp.anchorURL === true) {
+                    setTimeout(() => {
+                        window.location.hash = otherPageId;
+                    }, timeToAnchor);
+                }
+            }, 1500);
+        });
+    }
 }
