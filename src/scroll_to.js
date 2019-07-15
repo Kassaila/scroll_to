b3b8fe0a26cdbe2281219c9ab6cf143f110d0967
Kassaila <dmytro.winter@gmail.com>
2019-07-15 14:16:17 +0300

    Screen height fix - 15.07.2019-1

--- src/scroll_to.js
+++ src/scroll_to.js
@@ -3,7 +3,6 @@
  * 2018
  */
 // scroll to
-// scroll to
 function scrollTo(setProp) {
     // Default options | Настройки по умолчанию
     setProp.scrollDelay = setProp.scrollDelay !== undefined && typeof setProp.scrollDelay === 'number' && setProp.scrollDelay >= 0 ? setProp.scrollDelay : 600;
@@ -12,8 +11,8 @@ function scrollTo(setProp) {
     //scroll to anchor
     let scrollToAnchor = (target) => {
         let anchor = $(target).offset().top,
-            timeRate = Math.round(Math.abs($(window).scrollTop() - anchor) / screen.availHeight);
-        if (screen.availHeight * 0.75 < Math.abs($(window).scrollTop() - anchor)) {
+            timeRate = Math.round(Math.abs($(window).scrollTop() - anchor) / $(window).height());
+        if ($(window).height() * 0.75 < Math.abs($(window).scrollTop() - anchor)) {
             if (setProp.preloader && $('html').find('#' + setProp.preloaderId).length !== 0 && timeRate > 2) {
                 $('#' + setProp.preloaderId).fadeIn('normal').delay(setProp.scrollDelay * 2).fadeOut('slow');
                 $('html, body').stop();
@@ -36,8 +35,8 @@ function scrollTo(setProp) {
             let currentPageId = $(this).attr('href'),
                 timeToAnchor = scrollToAnchor(currentPageId);
             setTimeout(() => {
-                if (setProp.anchorURL === true) window.location.hash = currentPageId;
-                if (setProp.scrollToCallback !== undefined) setProp.scrollToCallback();
+                if (setProp.anchorURL) window.location.hash = currentPageId;
+                if (setProp.afterScrollCallback) setProp.afterScrollCallback();
             }, timeToAnchor);
         }
     });
@@ -49,8 +48,8 @@ function scrollTo(setProp) {
             setTimeout(() => {
                 let timeToAnchor = scrollToAnchor(otherPageId);
                 setTimeout(() => {
-                    if (setProp.anchorURL === true) window.location.hash = otherPageId;
-                    if (setProp.scrollToCallback !== undefined) setProp.scrollToCallback();
+                    if (setProp.anchorURL) window.location.hash = otherPageId;
+                    if (setProp.afterScrollCallback) setProp.afterScrollCallback();
                 }, timeToAnchor);
             }, 1500);
         });
